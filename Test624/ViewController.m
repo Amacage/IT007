9f5af74d3d4a078db29d5dfedc7c4da245bf469e
Amacage <mac@wolf-2.local>
2016-06-24 16:19:09 +0800

    Revert "Commit"
    This reverts commit 2554be46cb87d598dd053a51dc0da6e4aa03b54a.

--- Test624/ViewController.m
+++ Test624/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"%s",__FUNCTION__);
+    // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
